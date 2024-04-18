19f7e1fb72c20141a7966e44cd2b160a0cf2770e
Ceecosmo <153816310+Ceecosmo@users.noreply.github.com>
2024-04-18 14:11:58 +0100

    Update settings.py

--- logistic/settings.py
+++ logistic/settings.py
@@ -144,15 +144,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'portfolio/staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ORIGIN_WHITELIST = ['http://localhost:5173',
+CORS_ORIGIN_WHITELIST = ['pwelfarehome.onrender.com',
     #add other origins as needed
 ]
 
-
-INTERNAL_IPS = [
-    'localhost',
-    '127.0.0.1',
-]
 
 
 
