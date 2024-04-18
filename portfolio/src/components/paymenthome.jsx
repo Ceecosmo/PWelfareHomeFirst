87cdc34bfa69801da105325420e7f9b03c30b8d3
hp <hp@DESKTOP-8PLOS23>
2024-04-18 12:48:31 +0100

    first commit

---
+++ portfolio/src/components/paymenthome.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function paymenthome() {
+  return (
+    <>
+      <br></br> <br></br> <br></br><br></br> <br></br> <br></br>
+      <div className='text-center'>
+    <h6 className="display-6 fw-bold 1h-1 mb-3 ">Welcome to Priests' Welfare Home</h6>
+    
+<h4 className="fw-bold text-center">WE HONOUR YOUR PRESENCE</h4>
+<br></br> <br></br> 
+
+<a href="http://localhost:8000/initiate-payment/" className="btn btn-danger lg">PLEASE SUPPORT US<i className="fa fa-arrow-right ms-3 me-3"></i></a>
+</div>
+<br></br> <br></br> <br></br><br></br> <br></br> <br></br>  
+    </>
+  )
+}
+
+export default paymenthome 
+
+
