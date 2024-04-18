cb7ea61d04dfff8e80e9ea14ab66396f9870f86c
Ceecosmo <153816310+Ceecosmo@users.noreply.github.com>
2024-04-18 18:09:15 +0100

    Update urls.py

--- logistic/urls.py
+++ logistic/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.config import settings
+from django.config.urls.static import static
 
 from django.urls import path, include
 from django.urls import path, include
@@ -32,5 +34,6 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
   
 ]
-
+if settings.DEBUG:
+    urlpatterns+=static(settings.STATICFILES,document_root=settings.STATIC_ROOT)
 
